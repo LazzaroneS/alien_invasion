825fa0cbb2c8268a047b541bdeddfe8a5ee1fe6d
Atank <atank@atank.org>
2023-07-22 19:19:55 +0800

    add some functionalities

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
         """Initialize ship and set its initial location."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
         
         # load the ship image and retrieve its bounding rectangle
         self.image = pygame.image.load('images/redfighter0005.png')
@@ -14,16 +15,23 @@ class Ship:
         # Each new ship is situated at the central bottom of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a float number in the attribute x of the ship
+        self.x = float(self.rect.x)
+
         # Moving sign(Do not move initially)
         self.moving_right = False
         self.moving_left = False
         
     def update(self):
         """Reposition of the ship in accordance with the moving sign."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        # update the ship's x value instead of the rect object's x value.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update object 'rect' in accordance with self.x
+        self.rect.x = self.x
         
     def blitme(self):
         """Drawing the ship at the designated location."""
